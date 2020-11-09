f61ce150d3cb0c69aa57336b711172d146089898
Austin <business.mocksock@gmail.com>
2020-11-09 12:39:09 -0800

    Added basic files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SimpleHttpComponent } from './simple-http/simple-http.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimpleHttpComponent
   ],
   imports: [
     BrowserModule,
