334f320db4533a7251cb1adaead5530c3fde0e73
Austin <business.mocksock@gmail.com>
2020-11-10 12:52:01 -0800

    Added basic framework

--- src/app/simple-http/simple-http.component.ts
+++ src/app/simple-http/simple-http.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-simple-http',
   templateUrl: './simple-http.component.html',
-  styleUrls: ['./simple-http.component.css']
+  // styleUrls: ['./simple-http.component.css'] tutorial did not have this line
 })
 export class SimpleHttpComponent implements OnInit {
 
-  constructor() { }
+  data: Object;
+  loading: Boolean; 
+
+  constructor(private http: HttpClient) {
+    
+   }
 
   ngOnInit(): void {
   }
